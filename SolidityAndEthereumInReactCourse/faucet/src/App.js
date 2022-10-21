a43acacc2edc9f312ff8ac6b50b625f8c5e70f0e
Tadeja Kersic <tadeja.kersic@gmail.com>
2022-10-21 08:47:47 +0200

    base ui

--- SolidityAndEthereumInReactCourse/faucet/src/App.js
+++ SolidityAndEthereumInReactCourse/faucet/src/App.js
@@ -1,9 +1,38 @@
+import { useEffect } from "react";
+import "./App.css"
 
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      console.log(window.ethereum)
+      console.log(window.web3)
+    }
+
+    loadProvider()
+  }, [])
+
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button 
+            className="btn mr-2" 
+            onClick={async () => {
+              const accoutns = await window.ethereum.request({method: "eth_requestAccounts"})
+              console.log(accoutns)
+            }}
+          >
+            Enable Ethereum
+          </button>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
