4bb60a947a70aae34cf568a805f2700485290c8e
Tadeja Kersic <tadeja.kersic@gmail.com>
2022-11-06 20:18:34 +0100

    improve connect button

--- SolidityAndEthereumInReactCourse/marketplace-eth/components/common/navbar.js
+++ SolidityAndEthereumInReactCourse/marketplace-eth/components/common/navbar.js
@@ -1,15 +1,16 @@
 import Link from "next/link"
 import { useWeb3 } from "providers/web3"
+import Button from "./button"
 
 export default function Navbar() {
 
-    const { connect } = useWeb3()
+    const { connect, isLoading, isWeb3Loaded } = useWeb3()
 
     return (
       <section>
         <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
           <nav className="relative" aria-label="Global">
-            <div className="flex justify-between">
+            <div className="flex justify-between items-center">
               <div>
               <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Home
@@ -25,11 +26,22 @@ export default function Navbar() {
                 <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Wishlist
                 </Link>
-                <span
-                  onClick={connect}
-                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                    Connect
-                </span>
+                { isLoading ?
+                  <Button
+                    disabled={true}
+                    onClick={connect}>
+                      Loading...
+                  </Button> :
+                  isWeb3Loaded ?
+                  <Button
+                    onClick={connect}>
+                      Connect
+                  </Button> :
+                  <Button
+                    onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
+                    Install Metamask
+                  </Button>
+                }
               </div>
             </div>
           </nav>
